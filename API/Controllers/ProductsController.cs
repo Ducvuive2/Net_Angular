6b8bcc08540bee1fdefbebe16a47e27594d1bb5c
duc20062001@gmail.com <85180552+ducvuive@users.noreply.github.com>
2025-01-05 11:45:02 +0700

    Initial Project. Apply repository and seed data

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(IProductRepository repo) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await repo.GetProductsAsync();
+
+        if (products == null || !products.Any())
+            return NotFound("No products found");
+
+        return Ok(products);
+    }
+
+    [HttpGet("{id:int}")] // api/products/2
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        repo.AddProduct(product);
+
+        if (await repo.SaveChangeAsync())
+        {
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        return BadRequest("Cannot create this product");
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id))
+            return BadRequest("Cannot update this product");
+
+        repo.UpdateProduct(product);
+
+        if (await repo.SaveChangeAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Cannot update this product");
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        repo.DeleteProduct(product);
+
+        if (await repo.SaveChangeAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Cannot delete this product");
+    }
+
+    private bool ProductExists(int id)
+    {
+        return repo.ProductExists(id);
+    }
+}
